aaab246898a1b1070319c562f0808426d3e6777b
jjuni <wnsgml6440@naver.com>
2022-08-12 17:40:07 +0900

    게시글 조회, 게시글 수정(db로 안옮겨짐)

--- src/main/java/com/study/domain/post/PostController.java
+++ src/main/java/com/study/domain/post/PostController.java
@@ -1,8 +1,12 @@
 package com.study.domain.post;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,17 +17,42 @@ public class PostController {
 	private final PostService postService;
 	
 	//게시글 작성 페이지
-	@GetMapping("post/write.do")
-	public String openPostWrite(Model model) {
-		String title="제목",
-				content="내용",
-				writer="홍길동";
-		
-		model.addAttribute("t", title);
-		model.addAttribute("c", content);
-		model.addAttribute("w", writer);
-		
+	@GetMapping("/post/write.do")
+	public String openPostWrite(@RequestParam(value="id", required=false) final Long id, Model model) {
+		if(id != null) {
+			PostResponse post = postService.findPostById(id);
+			model.addAttribute("post", post);
+		}
 		return "post/write";
 	}
-
+	
+	//신규 게시글 생성
+	@PostMapping("/post/save.do")
+	public String savePost(final PostRequest params) {
+		postService.savePost(params);
+		return "redirect:/post/list.do";
+	}
+	
+	//게시글 리스트 페이지
+	@GetMapping("post/list.do")
+	public String openPostList(Model model) {
+		List<PostResponse> posts = postService.findAllPost();
+		model.addAttribute("posts", posts);
+		return "post/list";  // templates/post/list.html 
+	}
+	
+	//게시글 상세 페이지
+	@GetMapping("/post/view.do")
+	public String openPostView(@RequestParam final Long id, Model model) {
+		PostResponse post = postService.findPostById(id);
+		model.addAttribute("post", post);
+		return "post/view";
+	}
+	
+	  // 기존 게시글 수정
+    @PostMapping("/post/update.do")
+    public String updatePost(final PostRequest params) {
+        postService.updatePost(params);
+        return "redirect:/post/list.do";
+    }
 }
